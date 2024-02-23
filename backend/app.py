d09306b0293880fe34d873cacd30fdb105774d8b
Vangekillsvado <aclarke500@gmail.com>
2024-02-22 21:27:02 -0500

    edited paths to be relative

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,10 @@ VOCAB_PATH = "/home/marcuswrrn/Projects/QMIND/qmind-codenames-front-end/backend/
 BOARD_PATH = "/home/marcuswrrn/Projects/QMIND/qmind-codenames-front-end/backend/data/codenames_boards.json"
 MODEL_PATH = "/home/marcuswrrn/Projects/QMIND/qmind-codenames-front-end/backend/data/model.pth"
 
+VOCAB_PATH = "./data/words_extended.json"
+BOARD_PATH = "./data/codenames_boards.json"
+MODEL_PATH = "./data/model.pth"
+
 device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 print(f"Server Running on: {device}")
 
