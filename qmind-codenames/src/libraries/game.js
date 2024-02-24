9782cd1ff92c2da2e5b71ffe90f9bf8d3220cf6d
Vangekillsvado <aclarke500@gmail.com>
2024-02-24 17:42:25 -0500

    reprompt model on every move

--- qmind-codenames/src/libraries/game.js
+++ qmind-codenames/src/libraries/game.js
@@ -12,23 +12,77 @@ function endTurn(guessWord){
   if (guessWord == store.assassinWord || store.assassinWord[0]) return true;
 }
 
+
 /**
- * Determines the set of moves the computer is going to make
- * @returns {Array<String>} the list of moves the computer is making
+ * Takes the sorted board and returns the words for one computer move
+ * @param {Array<String>} computerGuesses sorted order of words fetched from API
+ * @returns {Array<String>} valid moves for the computer to make
  */
-function getComputerTurn(){
-  const computerMoves = [];
-  let turnOver = false;
-  while (!turnOver){
-    const guessWord = store.computerGuesses[store.computerGuessIndex];
-    const wordObj = getWordObject(guessWord);
-    if (!wordObj.flipped) { // don't make a move if human flipped the word
-      computerMoves.push(wordObj);
-      turnOver = endTurn(guessWord);
+function trimComputerMoves(computerGuesses){
+  const validGuesses = [];
+  const guessWordObjects =  computerGuesses.map(wordLiteral=>getWordObject(wordLiteral));
+
+  // add moves until computer makes a bad guess
+  for (let i = 0; i < guessWordObjects.length; i++){
+    if (guessWordObjects[i].wordType != 'teamTwo'){
+      return validGuesses;
     }
-    store.computerGuessIndex++;
+    validGuesses.push(guessWordObjects[i])
   }
-  return computerMoves;
+  return validGuesses;
+}
+
+/**
+ * Get words that haven't been selected yet
+ * @param {Array<Word>} wordObjects 
+ * @returns {Array<String>} word literals that haven't been flipped
+ */
+function getValidWords(wordObjects){
+  const validWords = [];
+  wordObjects.forEach((wordObj)=>{
+    if(!wordObj.flipped) validWords.push(wordObj.word)
+  })
+  return validWords;
+}
+
+/**
+ * Gets the state of game board formatted for the API call
+ * @returns {Object} formatted for the AI API
+ */
+function getCurrentGameBoard(){
+  return {
+    target_words:getValidWords(store.teamTwoWordObjects),
+    negative_words:getValidWords(store.teamOneWordObjects),
+    neutral_words:getValidWords(store.bystanderWordObjects),
+    assassin_word:store.assassinWord[0], // if assassin selected game is over -> assassin not selected
+  }
+}
+
+/**
+ * Grabs the AI's sorted board from the API
+ * and updates the store's hints
+ * @returns {Array<String>}
+ */
+async function fetchComputerMoves(){
+  const currentGameBoard = getCurrentGameBoard();
+  const response = await fetch('http://127.0.0.1:5000/prompt-model', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(currentGameBoard)
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  // not good form to update here...but who cares
+  store.aiHint = data.cpu_hint; 
+  store.hint = data.human_hint;
+  return data.similar_words;
+
 }
 
 /**
@@ -36,13 +90,15 @@ function getComputerTurn(){
  * Gets move and updates wordObjects/components.
  */
 export async function computerMove() {
-  const compMove = getComputerTurn();
+  // AI sorts the entire board
+  const sortedBoard = await fetchComputerMoves();
+  const compMove = trimComputerMoves(sortedBoard);
   // Wrap setTimeout in a promise
   const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
   for (let i = 0; i < compMove.length; i ++){
     await wait(250)
-    compMove[i].clicked()
+    compMove[i].clicked() // updates component/Obj
     await wait(500)
   }
 }
